9e8435dea4394d5558494f7481feb849f064f01a
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2023-01-19 10:13:03 +0100

    added the mongodb connection

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,10 @@
-import mongodb from 'mongodb';
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+    await mongoose.connect(process.env.MONGODB_URI, {
+        // this are just impostation to make more secure the connection and can be needed in older browsers
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    console.log("MongoDB connected ... ")
+}
